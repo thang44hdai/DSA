7bf8ec0e341454af2ddd2499b01999a636b52f34
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-05-16 10:06:41 +0700

    src

--- Dijstra- Floyd- Bellman/H.cpp
+++ Dijstra- Floyd- Bellman/H.cpp
@@ -1,56 +1,42 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-ll M = 1e9 + 7;
-ll n, m;
-
-ll d[1005];
-ll dp[1005] = {};// lưu số cách đi từ 1 đến i
-vector<pair<ll, ll>> vt[1005];
-priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<>> pq;
-void Dijkstra()
-{
-    d[1] = 0;
-    dp[1] = 1;
-    pq.push({d[1], 1});
-    while (!pq.empty())
-    {
-        pair<int, int> top = pq.top();
-        pq.pop();
-        if (top.first > d[top.second])
-            continue;
-        for (auto i : vt[top.second])
-        {
-            if (d[i.first] > d[top.second] + i.second)
-            {
-                d[i.first] = d[top.second] + i.second;
-                dp[i.first] = dp[top.second];
-                pq.push({d[i.first], i.first});
-            }
-            else if (d[i.first] == d[top.second] + i.second)
-            {
-                dp[i.first] = (dp[top.second] + dp[i.first]) % M;
-            }
-        }
-    }
-    cout << dp[n];
+bool cmp(string a, string b){
+	if(a.size()!=b.size()){
+		return a.size()<b.size();
+	}
+	return a<b;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> m;
-    while (m--)
-    {
-        ll x, y, z;
-        cin >> x >> y >> z;
-        vt[x].push_back({y, z});
-        vt[y].push_back({x, z});
-    }
-    fill(d, d + 1005, 10e9);
-    Dijkstra();
-
-    return 0;
+int main(){
+	string s;
+	cin>>s;
+	vector<string>vs;
+	s=s+"%";
+	string c="";
+	for(int i=0;i<s.size();i++){
+		if(isdigit(s[i])){
+			c=c+s[i];
+		} else {
+			if(c!=""){
+				vs.push_back(c);
+			}
+			c="";
+		}
+	}
+	for(int i=0;i<vs.size();i++){
+		int pos=-1e9;
+		for(int j=0;j<vs[i].size();j++){
+			if(vs[i][j]!='0'){
+				pos=j;
+				break;
+			}
+		}
+		if(pos!=-1e9){
+			vs[i].erase(0,pos);
+		}
+		if(pos==vs[i].size()-1){
+			vs[i]="0";
+		}
+	}
+	sort(vs.begin(),vs.end(), cmp); 
+	cout<<vs[vs.size()-1];
 }
--- Segment Tree and Fenwick Tree (BIT)/K.cpp
+++ Segment Tree and Fenwick Tree (BIT)/K.cpp
@@ -1,57 +1,8 @@
-#include <bits/stdc++.h>
-#pragma GCC optimize("Ofast")
-#define ll long long
-using namespace std;
-ll fen[1000005];
-ll a[60005];
-ll n;
-ll Max = 0;
-// update node cha thêm 1 phần tử để tạo cặp nghịch thế
-void Update(ll i)
-{
-    while (i <= Max)
-    {
-        fen[i]++;
-        i += i & -i;
-    }
-}
-// đếm số cặp nghịch thế trước nó
-ll get(ll i)
-{
-    ll tmp = 0;
-    while (i > 0)
-    {
-        tmp += fen[i];
-        i -= i & -i;
-    }
-    return tmp;
-}
-ll sol()
-{
-    ll ans = 0;
-    for (int i = n; i >= 1; i--)
-    {
-        ll len;
-        if (a[i] == 1)
-            len = 0;
-        else
-            len = get(a[i] - 1); 
-        ans += len;
-        Update(a[i]);
-    }
-    return ans;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n;
-    for (ll i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-        Max = max(Max, a[i]);
-    }
-    cout << sol();
+
+#include <stdio.h>
+int main(void){
+    
+    char *p = "Hello";
+    printf("%d\n", p);
     return 0;
 }
